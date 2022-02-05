9b169554ebd6f38b54036a767f2da828825954f3
Haroon <haroon20@hotmail.co.uk>
2022-02-05 12:26:22 +0000

    step 4 complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,13 @@
-describe Oystercard do
+require './lib/oystercard.rb'
 
+describe Oystercard do
+  describe "#balance" do
+    it "card has a balance" do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it "new oyster card has a 0 balance" do
+      expect(subject.balance).to eq 0
+    end
+end
 end
