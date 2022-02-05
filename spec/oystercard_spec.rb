675da565811b676ab973614303ed399a3c38ad6b
Rakshak Shetty <rakshak_81@hotmail.com>
2022-02-05 12:51:04 +0000

    Step 4

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,5 +9,16 @@ describe Oystercard do
     it "new oyster card has a 0 balance" do
       expect(subject.balance).to eq 0
     end
+  end
+
+  describe "#top_up" do
+    it "Card should be able to top up" do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+
+    it "top_up(5) should increase balance by £5" do
+      expect{ subject.top_up(5) }.to change{ subject.balance }.by (5)
+    end
+  end
 end
-end
+
